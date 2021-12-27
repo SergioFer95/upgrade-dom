6660a74b3b379c3dfaee75b1087bbbf3e1974a2c
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2021-12-27 10:21:23 +0100

    Do Iteration #1

--- style.js
+++ style.js
@@ -1,2 +1,22 @@
 'use strict';
 
+window.addEventListener("load", () =>{
+    var btn = document.querySelector(".showme");
+    console.log(btn);
+    
+    var first = document.querySelector("#pillado");
+    console.log(first);
+
+    var showParagraph = document.querySelectorAll("p");
+    console.log(showParagraph);
+
+    var pokemon = document.querySelectorAll(".pokemon");
+    console.log(pokemon);
+
+    var atribute = document.querySelectorAll('[data-function="testMe"]');
+    for(var i = 0; i <= atribute.length; i++){
+        console.log(atribute[i]);
+    }
+    
+    console.log(atribute[2]);
+})
